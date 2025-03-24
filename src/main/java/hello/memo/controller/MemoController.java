c48cfbc0a802834682c178b90eb6737071554439
devgogogogo <imleo322@gmail.com>
2025-03-24 18:05:40 +0900

    CRUD의 예외처리를 추가하였음.(강의5-10강)

--- src/main/java/hello/memo/controller/MemoController.java
+++ src/main/java/hello/memo/controller/MemoController.java
@@ -3,11 +3,11 @@ package hello.memo.controller;
 import hello.memo.dto.MemoRequestDto;
 import hello.memo.dto.MemoResponseDto;
 import hello.memo.entity.Memo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/memos")
@@ -17,7 +17,8 @@ public class MemoController {
     private final Map<Long, Memo> memoList = new HashMap<>();
 
     @PostMapping
-    public MemoResponseDto createMemo(@RequestBody MemoRequestDto dto) {
+    public ResponseEntity<MemoResponseDto> createMemo(@RequestBody MemoRequestDto dto)
+    {
 
         // 식별자가 1씩 증가 하도록 만들어야 한다.
         Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
@@ -28,27 +29,83 @@ public class MemoController {
         //우리가 만들어준 리스트(memoList) DB에 Memo 메모
         memoList.put(memoId, memo);
 
-        return new MemoResponseDto(memo);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}")
-    public MemoResponseDto findMemoById(@PathVariable Long id ) {
+    @GetMapping // 메모 목록 조회
+    public List<MemoResponseDto> findAllMemos() {
+        // 전체 조회이기 때문에 라마미터로 받을 데이터가 없다.
+        //init List
+        List<MemoResponseDto> responseList = new ArrayList<>();
+
+        // HashMap<Memo> -> List<MemoResponseDto>
+        for (Memo memo : memoList.values()) {
+            MemoResponseDto dto = new MemoResponseDto(memo);
+            responseList.add(dto);
+        }
+        // Map to List
+//        memoList.values().stream().map(MemoResponseDto::new).toList();
+//       위에 40번째 줄에 있는 것과 같은것임 다르게 표현 -->가독성을 위해 위에 있는것을 사용
+        return responseList;
+    }
+
+    @GetMapping("/{id}") //메모 단건 조회
+    public ResponseEntity<MemoResponseDto>  findMemoById(@PathVariable Long id ) {
         Memo memo = memoList.get(id);
 
-        return new MemoResponseDto(memo);
+        if (memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK) ;
     }
 
     @PutMapping("/{id}")
-    public MemoResponseDto updateMemoById(@PathVariable long id, @RequestBody MemoRequestDto dto) {
+    public ResponseEntity<MemoResponseDto> updateMemoById(@PathVariable long id, @RequestBody MemoRequestDto dto)
+    {
         Memo memo = memoList.get(id);
+
+        //NPE 방지
+        if (memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        //필수값 검증
+        if (dto.getTitle() == null || dto.getContents() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        // memo 수정
         memo.update(dto);
 
-        return new MemoResponseDto(memo);
+        //응답
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<MemoResponseDto> updateTitle(@PathVariable Long id, @RequestBody MemoRequestDto dto
+    ) {
+        Memo memo = memoList.get(id);
+
+        //NPE 방지
+        if (memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        // 필수값 검증
+        if (dto.getTitle() == null || dto.getContents() != null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        memo.updateTitle(dto);
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public String deleteMemoById(@PathVariable Long id) {
-        memoList.remove(id);
-        return "삭제 되었습니다";
+    public ResponseEntity<Void> deleteMemoById(@PathVariable Long id) {
+
+        if (memoList.containsKey(id)) { //memoList의 Key값에 id를 포함하고 있다면
+            memoList.remove(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
